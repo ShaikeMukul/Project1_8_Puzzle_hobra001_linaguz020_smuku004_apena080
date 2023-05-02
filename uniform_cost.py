708f7e5c96c561fee13966ea47baeebe47ae1767
ShaikeMukul <smuku004@ucr.edu>
2023-05-01 20:55:40 -0700

    uniformcost class done

---
+++ uniform_cost.py
@@ -0,0 +1,46 @@
+from queue import PriorityQueue
+
+class UniformCost:
+    def get_neighbors(self, state):
+        neighbors = []
+        index = state.index('0')
+        if index % 3 > 0:
+            # Move left
+            neighbor = state[:index - 1] + '0' + state[index - 1] + state[index + 1:]
+            neighbors.append(neighbor)
+        if index % 3 < 2:
+            # Move right
+            neighbor = state[:index] + state[index + 1] + '0' + state[index + 2:]
+            neighbors.append(neighbor)
+        if index // 3 > 0:
+            # Move up
+            neighbor = state[:index - 3] + '0' + state[index - 2:index] + state[index - 3] + state[index + 1:]
+            neighbors.append(neighbor)
+        if index // 3 < 2:
+            # Move down
+            neighbor = state[:index] + state[index + 3] + state[index + 1:index + 3] + '0' + state[index + 4:]
+            neighbors.append(neighbor)
+        return neighbors
+
+    def uniform_cost_search(self, initial_state, goal_state):
+        frontier = PriorityQueue()
+        frontier.put((0, initial_state))
+        explored = set()
+        max_queue_size = 0
+        nodes_expanded = 0
+        while not frontier.empty():
+            cost, state = frontier.get()
+            nodes_expanded += 1
+            if state == goal_state:
+                print("Goal!!!")
+                print("To solve this problem the search algorithm expanded a total of:", nodes_expanded, "nodes.")
+                print("The maximum number of nodes in the queue at any one time:", max_queue_size)
+                print("The depth of the goal node was:", cost)
+                return cost
+            explored.add(state)
+            for neighbor in self.get_neighbors(state):
+                if neighbor not in explored:
+                    frontier.put((cost + 1, neighbor))
+            if frontier.qsize() > max_queue_size:
+                max_queue_size = frontier.qsize()
+        return -1
