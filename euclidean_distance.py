cf17d164f155a8cbbcec875f1db368c0a6209528
ShaikeMukul <smuku004@ucr.edu>
2023-05-01 20:56:46 -0700

    class init

---
+++ euclidean_distance.py
@@ -0,0 +1,3 @@
+class EuclideanDistance:
+    def a_star_with_euclidean_distance_heuristic(self):
+        pass
---
+++ misplaced_tile.py
@@ -0,0 +1,4 @@
+
+class MisplacedTile:
+    def a_star_with_misplaced_tile_heuristic(self):
+        pass
