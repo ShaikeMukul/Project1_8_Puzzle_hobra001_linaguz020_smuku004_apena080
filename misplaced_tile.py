872fa5859326b5ea66340a809c02c419289968cc
linaguz020 <linaguz020@gmail.com>
2023-05-06 16:13:51 -0700

    misplaced prints results, checking for accuracy

--- misplaced_tile.py
+++ misplaced_tile.py
@@ -5,7 +5,7 @@ from queue import PriorityQueue
 class MisplacedTile:
     def get_neighbors(self, state):
         neighbors = []
-        index = state.index('0') // 3
+        index = state.index('0')
         if index % 3 > 0:
             # Move left
             neighbor = state[:index - 1] + '0' + state[index - 1] + state[index + 1:]
@@ -36,19 +36,22 @@ class MisplacedTile:
     # TODO: fix for loop
     def a_star_with_misplaced_tile_heuristic(self, initial_state, goal_state):
         frontier = PriorityQueue()
-        frontier.put((initial_state, 0))
-        explored = set()
-        expanded = 0
+        frontier_set = set() # use to keep track of items in the queue
+        frontier.put((initial_state, 0)) # initalize frontier with initial state
+        frontier_set.add((initial_state, 0))
+        explored = set() # initialize explored set to be empty
+        expanded = 0 # keep track of expanded nodes
         while not frontier.empty():
-            state, cost = frontier.get()
+            state, cost = frontier.get() # choose a leaf node, also removes from queue
             expanded += 1
-            if state == goal_state:
+            if state == goal_state: # if node contains goal state
                 print("Goal!!!")
                 print("To solve this problem the search algorithm expanded a total of:", expanded, "nodes.")
                 print("The depth of the goal node was:", cost)
                 return cost
-            explored.add(state)
-            for neighbor in self.get_neighbors(state):
-                if neighbor not in explored:
+            explored.add(state) # add node to explored set
+            for neighbor in self.get_neighbors(state): # expand chosen node
+                if neighbor not in explored and neighbor not in frontier_set: # add resulting nodes to frontier
                    frontier.put((neighbor, self.num_misplaced(neighbor, goal_state) + 1)) 
-        return -1
+                   frontier_set.add((neighbor, self.num_misplaced(neighbor, goal_state) + 1))
+        return -1 #if frontier is empty return -1
